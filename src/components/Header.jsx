b7a0a19c9504c3cfcada353aa0453810a176dcc5
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-09-15 23:27:41 +0200

    feat: add budgetMode-btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,17 +7,20 @@ const Header = () => {
 			<div className="container-fluid">
 				<div className="row">
 					<div className="col-12">
-						<ul className="list-unstyled d-flex">
-							<li className="me-3">
-								<NavLink to="/">Homepage</NavLink>
-							</li>
-							<li className="me-3">
-								<NavLink to="/aboutus">Chi siamo</NavLink>
-							</li>
-							<li className="me-3">
-								<NavLink to="/products">Prodotti</NavLink>
-							</li>
-						</ul>
+						<div className="d-flex justify-content-between">
+							<ul className="list-unstyled d-flex">
+								<li className="me-3">
+									<NavLink to="/">Homepage</NavLink>
+								</li>
+								<li className="me-3">
+									<NavLink to="/aboutus">Chi siamo</NavLink>
+								</li>
+								<li className="me-3">
+									<NavLink to="/products">Prodotti</NavLink>
+								</li>
+							</ul>
+							<button className="btn btn-primary">Budget Mode</button>
+						</div>
 					</div>
 				</div>
 			</div>
